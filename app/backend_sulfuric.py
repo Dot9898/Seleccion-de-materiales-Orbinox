02480fa877e56dca84084d6789d85046f8c10a7b
Diego <diegoorellanadoctors@gmail.com>
2026-03-15 02:08:21 -0300

    Graph details

--- app/backend_sulfuric.py
+++ app/backend_sulfuric.py
@@ -11,6 +11,13 @@ X_MIN = 5
 X_MAX = 100 #125
 Y_MIN = 0
 Y_MAX = 180
+HOVERED_CURVE_OPACITY = 1.0 #unused
+NON_HOVERED_CURVE_OPACITY = 0.4 #unused
+HOVERED_CURVE_THICKNESS = 2.6
+NON_HOVERED_CURVE_THICKNESS = 1.6
+AREA_UNDER_CURVE_OPACITY = 0.15
+
+
 ALIASES = {}
 
 
@@ -55,17 +62,35 @@ def load_all_curves_data():
     all_curves_data = pd.concat(curves_data)
     return(all_curves_data)
 
-@st.cache_resource
-def load_graph():
+def generate_graph():
     all_curves_data = load_all_curves_data()
     altair.data_transformers.disable_max_rows()
-    chart = altair.Chart(all_curves_data)
-    chart = chart.mark_line(clip = True)
-    #chart = chart.transform_filter((altair.datum.x >= X_MIN) & (altair.datum.x <= X_MAX))
-    chart = chart.encode(x = altair.X('x', scale = altair.Scale(domain = [X_MIN, X_MAX])))
-    chart = chart.encode(y = altair.Y('y', scale = altair.Scale(domain = [Y_MIN, Y_MAX])))
-    chart = chart.encode(color = 'material')
-    return(chart)
+
+    base_chart = altair.Chart(all_curves_data)
+    base_chart = base_chart.transform_filter(altair.FieldOneOfPredicate(field = 'material', oneOf = ['AISI 316', '904 L']))
+    base_chart = base_chart.transform_filter(altair.datum.x <= X_MAX)
+
+    base_chart = base_chart.encode(x = altair.X('x', scale = altair.Scale(domain = [X_MIN, X_MAX])))
+    base_chart = base_chart.encode(y = altair.Y('y', scale = altair.Scale(domain = [Y_MIN, Y_MAX])))
+    base_chart = base_chart.encode(color = 'material')#, detail = 'material')
+
+    hovered_curve = altair.selection_point(on = 'pointerover', fields = ['material'], empty = False)
+    clicked_curve = altair.selection_point(fields = ['material'], on = 'click', empty = False)
+    
+    points = base_chart.mark_circle(size = 6000, opacity = 0)
+    points = points.add_params(hovered_curve)
+
+    lines = base_chart.mark_line(clip = True)
+    lines = lines.encode(strokeWidth = altair.condition(hovered_curve, altair.value(HOVERED_CURVE_THICKNESS), altair.value(NON_HOVERED_CURVE_THICKNESS)))
+    #lines = lines.encode(opacity = altair.condition(hover, altair.value(HOVERED_CURVE_OPACITY), altair.value(NON_HOVERED_CURVE_OPACITY)))
+    lines = lines.add_params(clicked_curve)
+
+    area = base_chart.transform_filter(clicked_curve)
+    area = area.mark_area(opacity = AREA_UNDER_CURVE_OPACITY, clip = True)
+    
+    full_chart = points + lines + area
+    return(full_chart)
+
 
 
 
--- app/frontend_sulfuric.py
+++ app/frontend_sulfuric.py
@@ -1,17 +1,14 @@
 
 
 import streamlit as st
-from backend_sulfuric import load_graph
+from backend_sulfuric import generate_graph
 
 CHART_HEIGHT = 600 #'stretch'
 
 st.set_page_config(layout = 'wide')
 
-graph = load_graph()
+graph = generate_graph()
 st.altair_chart(graph, width = 'stretch', height = CHART_HEIGHT)
-
-
-
 
 
 
