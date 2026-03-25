ce9d1be69298800ffc9fe09c5a93ce5f0cfb31f2
Diego <diegoorellanadoctors@gmail.com>
2026-03-25 08:15:02 -0300

    typo

--- app/entry_point.py
+++ app/entry_point.py
@@ -8,7 +8,7 @@ ROOT_PATH = Path(__file__).resolve().parent.parent
 screen_mode = get_screen_mode()
 if screen_mode == 'desktop':
     materials_page = st.Page(ROOT_PATH / 'app' / 'materials_desktop.py', title = 'Materiales')
-    sulfuric_page = st.Page(ROOT_PATH / 'app' / 'acid_mobile.py', title = 'Ácido sulfúrico')
+    sulfuric_page = st.Page(ROOT_PATH / 'app' / 'acid_desktop.py', title = 'Ácido sulfúrico')
 if screen_mode == 'mobile':
     materials_page = st.Page(ROOT_PATH / 'app' / 'materials_mobile.py', title = 'Materiales')
     sulfuric_page = st.Page(ROOT_PATH / 'app' / 'acid_mobile.py', title = 'Ácido sulfúrico')
